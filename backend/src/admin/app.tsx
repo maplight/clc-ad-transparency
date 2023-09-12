bc4d6cdb041066b5d58e157681d4e4e890088089
Harlie Levine <harlie.levine@gmail.com>
2023-09-12 12:26:40 -0400

    Adding a few new translations and instructions on updating

---
+++ backend/src/admin/app.tsx
@@ -0,0 +1,15 @@
+export default {
+  config: {
+    locales: ['en'],
+    translations: {
+      en: {
+        "app.components.LeftMenu.navbrand.title": "Ad Transparency DB",
+        "Auth.form.welcome.title": "CLC Ad Transparency DB",
+        "Auth.form.welcome.subtitle": "Log in to your account",
+      }
+    },
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
