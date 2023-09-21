ce96d3761f7f6d72edd657870dbd55de19885eae
Cory Alvernaz <cory@22labs.tech>
2023-09-20 20:20:36 -0600

    Index ad disclosures when reports are created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,10 @@
+import algoliaClient from './utils/algolia-client';
+
+const AD_DISCLOSURE_MODEL_UID = 'api::ad-disclosure.ad-disclosure';
+const REPORT_MODEL_UID = 'api::report.report';
+
+const adDisclosuresIndex = algoliaClient.initIndex(`ad_disclosures_${process.env.NODE_ENV}`);
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -14,5 +21,31 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
-};
+  bootstrap({ strapi }) {
+    strapi.db.lifecycles.subscribe({
+      models: [REPORT_MODEL_UID],
+
+      async afterCreate(event) {
+        const { adDisclosures: reportAdDisclosures } = event.result;
+        const reportAdDisclosureIds = JSON.parse(reportAdDisclosures).map(({ id }) => id);
+
+        const adDisclosures = await strapi.entityService.findMany(AD_DISCLOSURE_MODEL_UID, {
+          filters: {
+            id: reportAdDisclosureIds,
+          }
+        });
+
+        const adDisclosureObjects = adDisclosures.map(({ id, ...adDisclosure }) => ({
+          objectID: id,
+          ...adDisclosure,
+        }));
+
+        try {
+          await adDisclosuresIndex.saveObjects(adDisclosureObjects);
+        } catch (error) {
+          console.error('Error saving ad disclosures to Algolia', error);
+        }
+      },
+    });
+  },
+}
