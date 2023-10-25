c43cc5654fc6aecda54901d3cfe36a2420c94cea
Cory Alvernaz <cory@22labs.tech>
2023-10-25 15:12:19 -0600

    Add basic instant search
    Uses the Algolia Remix guide to stub out a basic search box:
    https://www.algolia.com/doc/guides/building-search-ui/going-further/server-side-rendering/react/#with-remix

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,41 +1,76 @@
-import type { MetaFunction } from "@vercel/remix";
+import { renderToString } from "react-dom/server";
+import type { InstantSearchServerState } from "react-instantsearch";
+import { type LoaderFunction, type MetaFunction, json } from "@vercel/remix";
+import {
+  InstantSearch,
+  InstantSearchSSRProvider,
+  SearchBox,
+  getServerState,
+  Hits,
+} from "react-instantsearch";
+import { useLoaderData } from "@remix-run/react";
+import { history } from "instantsearch.js/cjs/lib/routers/index.js";
+import searchClient from "~/search-client";
+
+import "instantsearch.css/themes/satellite.css";
+
+type SearchProps = {
+  serverState?: InstantSearchServerState;
+  serverUrl?: string;
+};
+
+const Search = ({ serverState, serverUrl }: SearchProps) => {
+  return (
+    <InstantSearchSSRProvider {...serverState}>
+      <InstantSearch
+        searchClient={searchClient}
+        indexName={`ad_disclosures_${process.env.NODE_ENV}`}
+        routing={{
+          router: history({
+            getLocation() {
+              if (typeof window === "undefined") {
+                return new URL(serverUrl!) as unknown as Location;
+              }
+
+              return window.location;
+            },
+          }),
+        }}
+      >
+        <SearchBox />
+        <Hits />
+      </InstantSearch>
+    </InstantSearchSSRProvider>
+  );
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const serverUrl = request.url;
+  const serverState = await getServerState(<Search serverUrl={serverUrl} />, {
+    renderToString,
+  });
+
+  return json({
+    serverState,
+    serverUrl,
+  });
+};
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "CLC Ad Transparency Database" },
+    {
+      name: "description",
+      content: "Welcome to the CLC Ad Transparency Database!",
+    },
   ];
 };
 
 export default function Index() {
+  const { serverState, serverUrl } = useLoaderData() as SearchProps;
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <Search serverState={serverState} serverUrl={serverUrl} />
     </div>
   );
 }
