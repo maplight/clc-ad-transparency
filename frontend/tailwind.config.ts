81b182100a80c7b17653bad9c01e0660dde1d109
Cory Alvernaz <cory@22labs.tech>
2023-11-07 10:56:42 -0700

    Add styling for `NumericMenu`

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors";
+import forms from "@tailwindcss/forms";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
@@ -20,5 +21,5 @@ export default {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [forms],
 } satisfies Config;
