59a90c0ef003d9b0577a833e5b3d4cd3f0e9220c
Cory Alvernaz <cory@22labs.tech>
2023-11-13 20:13:16 -0700

    Tighten up card layout and style sidebar

--- frontend/app/components/Card/index.tsx
+++ frontend/app/components/Card/index.tsx
@@ -8,11 +8,11 @@ type Props = {
 
 const Card = ({ children, title }: Props): ReactElement => {
   return (
-    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-      <div className="px-2 py-3 sm:px-4">
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-lg">
+      <div className="px-1 py-2 sm:px-3">
         <h3 className="text-lg font-medium text-gray-900">{title}</h3>
       </div>
-      <div className="px-2 py-3 sm:p-4">{children}</div>
+      <div className="px-1 py-2 sm:p-3">{children}</div>
     </div>
   );
 };
--- frontend/app/components/Search/FiltersSidebar/SearchFilters/index.tsx
+++ frontend/app/components/Search/FiltersSidebar/SearchFilters/index.tsx
@@ -5,82 +5,78 @@ import Card from "~/components/Card";
 
 const SearchFilters = () => {
   return (
-    <ul className="flex flex-1 flex-col gap-y-7">
+    <ul className="-mx-2 space-y-1 flex flex-1 flex-col gap-y-2">
       <li>
-        <ul className="-mx-2 space-y-1">
-          <li>
-            <Card title="Election">
-              <Menu
-                attribute="adElection"
-                classNames={{
-                  selectedItem: "font-black text-primary-500",
-                }}
-                sortBy={["count:desc", "name:asc", "isRefined:asc"]}
-              />
-            </Card>
-          </li>
-          <li>
-            <Card title="Format">
-              <MenuSelect
-                attribute="adFormat"
-                sortBy={["count:desc", "name:asc", "isRefined:asc"]}
-              />
-            </Card>
-          </li>
-          <li>
-            <Card title="Filer">
-              <RefinementList
-                attribute="createdBy"
-                searchable
-                sortBy={["count:desc", "name:asc", "isRefined:asc"]}
-              />
-            </Card>
-          </li>
-          <li>
-            <Card title="Ad Spend">
-              <NumericMenu
-                attribute="adSpend"
-                items={[
-                  { label: "Any", start: 0 },
-                  { label: "$1,000+", start: 1000 },
-                  { label: "$10,000+", start: 10000 },
-                  { label: "$100,000+", start: 100000 },
-                  { label: "$1,000,000+", start: 1000000 },
-                ]}
-              />
-            </Card>
-          </li>
-          <li>
-            <Card title="Candidate">
-              <HierarchicalMenu
-                attributes={["candidates.lvl0", "candidates.lvl1"]}
-                classNames={{
-                  selectedItem: "font-black text-primary-500",
-                }}
-              />
-            </Card>
-          </li>
-          <li>
-            <Card title="Measure">
-              <HierarchicalMenu
-                attributes={["measures.lvl0", "measures.lvl1"]}
-                classNames={{
-                  selectedItem: "font-black text-primary-500",
-                }}
-              />
-            </Card>
-          </li>
-          <li>
-            <Card title="Party">
-              <HierarchicalMenu
-                attributes={["politicalParties.lvl0", "politicalParties.lvl1"]}
-                classNames={{
-                  selectedItem: "font-black text-primary-500",
-                }}
-              />
-            </Card>
-          </li>
-        </ul>
+        <Card title="Election">
+          <Menu
+            attribute="adElection"
+            classNames={{
+              selectedItem: "font-black text-primary-500",
+            }}
+            sortBy={["count:desc", "name:asc", "isRefined:asc"]}
+          />
+        </Card>
+      </li>
+      <li>
+        <Card title="Format">
+          <MenuSelect
+            attribute="adFormat"
+            sortBy={["count:desc", "name:asc", "isRefined:asc"]}
+          />
+        </Card>
+      </li>
+      <li>
+        <Card title="Filer">
+          <RefinementList
+            attribute="createdBy"
+            searchable
+            sortBy={["count:desc", "name:asc", "isRefined:asc"]}
+          />
+        </Card>
+      </li>
+      <li>
+        <Card title="Ad Spend">
+          <NumericMenu
+            attribute="adSpend"
+            items={[
+              { label: "Any", start: 0 },
+              { label: "$1,000+", start: 1000 },
+              { label: "$10,000+", start: 10000 },
+              { label: "$100,000+", start: 100000 },
+              { label: "$1,000,000+", start: 1000000 },
+            ]}
+          />
+        </Card>
+      </li>
+      <li>
+        <Card title="Candidate">
+          <HierarchicalMenu
+            attributes={["candidates.lvl0", "candidates.lvl1"]}
+            classNames={{
+              selectedItem: "font-black text-primary-500",
+            }}
+          />
+        </Card>
+      </li>
+      <li>
+        <Card title="Measure">
+          <HierarchicalMenu
+            attributes={["measures.lvl0", "measures.lvl1"]}
+            classNames={{
+              selectedItem: "font-black text-primary-500",
+            }}
+          />
+        </Card>
+      </li>
+      <li>
+        <Card title="Party">
+          <HierarchicalMenu
+            attributes={["politicalParties.lvl0", "politicalParties.lvl1"]}
+            classNames={{
+              selectedItem: "font-black text-primary-500",
+            }}
+          />
+        </Card>
       </li>
     </ul>
   );
