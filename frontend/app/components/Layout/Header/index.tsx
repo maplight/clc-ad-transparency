a15955c773091ed8608d3fa29f7403c84a929134
Cory Alvernaz <cory@22labs.tech>
2023-11-16 09:44:29 -0700

    Replace header image

--- frontend/app/components/Layout/Header/index.tsx
+++ frontend/app/components/Layout/Header/index.tsx
@@ -1,18 +1,18 @@
 import type { ReactElement } from "react";
-import clcLogo from "~/images/clc-logo.png";
+import mapLightLogo from "~/images/maplight-logo.png";
 
 const Header = (): ReactElement => {
   return (
-    <header className="bg-primary-600 fixed z-50 shadow-2xl w-full h-24">
+    <header className="bg-primary-600 fixed z-50 shadow-2xl w-full h-20">
       <div className="py-4 px-4 sm:px-6 lg:px-8">
-        <img className="h-12 w-auto" src={clcLogo} alt="CLC Logo" />
+        <img className="h-12 w-auto" src={mapLightLogo} alt="MapLight Logo" />
       </div>
     </header>
   );
 };
 
 const HeaderSpacer = (): ReactElement => {
-  return <div className="h-24" />;
+  return <div className="h-20" />;
 };
 
 export default Header;
